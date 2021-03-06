712a9249b40bbd1a2efc3ae8c467912bf7dbca7e
Iromanov_Almaz <mpakxxj@gmail.com>
2021-03-06 15:10:30 +0300

    edit destructure object

--- destructure_object.js
+++ destructure_object.js
@@ -12,10 +12,14 @@ const option = {
         bg: 'red'
     },
     opacity: 100,
-    gradient: true
-
-
+    gradient: true,
+    makeTest: function() {
+        console.log("test");
+    }
 };
+option.makeTest();
+
+console.log(Object.keys(option).length); //показывает число свойств объекта, но не видит свойства вложенного объекта. без length выводит их название
 
 // console.log(option["colors"]["border"]); //доступ к вложенному значению свойства через двойные квадратные скобки
 
@@ -30,15 +34,19 @@ const option = {
 
 
 // }
-//перебор внутри перебора
-for (let key in option) {
-    if (typeof (option[key]) === 'object') {
-        console.log(`найден вложенный объект - ${key}`);
-        for (let i in option[key]) {
-            console.log(`Свойство ${i} вложенного объекта ${key} имеет значение ${option[key][i]}`);
-        }
-    } else {
-        console.log(`Свойство ${key} имеет значение ${option[key]}`); // перебирает все свойства объекта, но не умеет перебирать вложенные объекты при таком запросе
-    }
 
-}
+// let counter = 0;
+// // перебор внутри перебора
+// for (let key in option) {
+//     if (typeof(option[key]) === 'object') {
+//         console.log(`найден вложенный объект - ${key}`);
+//         for (let i in option[key]) {
+//             console.log(`Свойство ${i} вложенного объекта ${key} имеет значение ${option[key][i]}`);
+//             counter++
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${option[key]}`); // перебор вложенных объектов
+//         counter++
+//     }
+// }
+// console.log(counter); //счетчик колличества свойств объекта
